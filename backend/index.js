8682ddeabf6a1a8045b4fedd13503af5a9f35484
Abdul Kadir <abdulkadir80949@gmail.com>
2025-11-27 06:13:53 +0530

    url fixed

--- backend/index.js
+++ backend/index.js
@@ -20,7 +20,7 @@ const __dirname = path.resolve();
 
 app.use(
   cors({
-     origin:"http://localhost:5173",
+     origin:"https://languagelearningplatform.onrender.com",
     credentials: true,
   })
 );
