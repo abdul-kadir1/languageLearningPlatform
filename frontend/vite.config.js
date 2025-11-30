f64b8c322cde07cd819f2221861e107db63b5068
Abdul Kadir <abdulkadir80949@gmail.com>
2025-11-30 12:03:11 +0530

    vite error fixed

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,26 +1,48 @@
+
+
+
 // import { defineConfig } from 'vite'
+// import tailwindcss from '@tailwindcss/vite'
 // import react from '@vitejs/plugin-react'
+// import daisyui from 'daisyui'
 
 // // https://vite.dev/config/
 // export default defineConfig({
-//   plugins: [react()],
+//   base:'/',
+//   plugins: [react(),
+             
+//               tailwindcss(),
+//               ('daisyui'),
+        
+
+//   ],
+ 
 // })
 
 
 import { defineConfig } from 'vite'
-import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 import daisyui from 'daisyui'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base:'/',
-  plugins: [react(),
-             
-              tailwindcss(),
-              ('daisyui'),
-        
-
+  base: '/',
+  plugins: [
+    react(),
+    tailwindcss(),
+    
   ],
- 
+  server: {
+    port: 5002,
+    proxy: {
+      '/api': {
+        target: 'https://languagelearningplatform.onrender.com/signup',
+        changeOrigin: true
+      }
+    }
+  },
+  build: {
+    outDir: '../backend/frontend/dist'
+  }
 })
